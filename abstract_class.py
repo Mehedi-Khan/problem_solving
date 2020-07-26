763e528270cd08a600700e383eafd82b031dc034
Md. Mehedi Khan <mehedidumis@gmail.com>
2020-07-26 14:20:34 +0600

    coded in abstract_class.py

--- abstract_class.py
+++ abstract_class.py
@@ -1,1 +1,25 @@
+# Defining Book class
+class Book:
+    def __init__(self, title, author, price=0):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    # Abstract display method to return title, author, price
+    def abstract_display(self):
+        print(f"Title: {self.title}", f"Author: {self.author}", f"Price: {self.price}", sep="\n")
+
+
+# Defining MyBook class
+class MyBook(Book):
+    def __init__(self, title, author, price=0):
+        Book.__init__(self, title, author, price)
+
+
+t = input()
+a = input()
+p = input()
+b = MyBook(t, a, p)
+b.abstract_display()
+
 
